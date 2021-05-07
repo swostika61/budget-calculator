e54ffcfb068369760705f7faf7d28e3d24e278f0
swostika61 <swostikalamichhane61@gmail.com>
2021-05-07 13:19:09 +0545

    use useState, reduce, map

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
+import React, { useState } from 'react';
 import './App.css';
 import { Alert } from './components/Alert';
 import { ExpenseForm } from './components/ExpenseForm';
 import ExpenseList from './components/ExpenseList';
+import { v4 as uuid } from 'uuid';
+
+const initialExpenses = [
+  {
+    id: uuid(),
+    charge: 'rent',
+    amount: 1600
+  },
+  {
+    id: uuid(),
+    charge: 'car',
+    amount: 400
+  },
+  {
+    id: uuid(),
+    charge: 'credit card bill',
+    amount: 1200
+  }
+]
 
 function App() {
+  const [expenses, setExpenses] = useState(initialExpenses)
+
   return (
     <>
-      helloe from me
       <Alert />
-      <ExpenseForm />
-      <ExpenseList />
+      <h1>budget calculator</h1>
+      <main className="App">
+        <ExpenseForm />
+        <ExpenseList expenses={expenses} />
+      </main>
+      <h1>total spending: <span className="total">${expenses.reduce((acc,curr)=>{
+        return acc +=curr.amount
+      },0)}</span> </h1>
     </>
   );
 }
